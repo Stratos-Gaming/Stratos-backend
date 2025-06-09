0e76767ddfd7d533ec70ff519c87cdc04ef64c68
jacopicci <jacopicci@gmail.com>
2025-06-09 13:08:54 +0200

    Update admin.py

--- backendStratos/userModule/admin.py
+++ backendStratos/userModule/admin.py
@@ -1,1 +1,7 @@
 from django.contrib import admin
+from .models import StratosUser, UserType, UserSubscriptionPreferences, UserSocialConnection
+
+admin.site.register(StratosUser)
+admin.site.register(UserType)
+admin.site.register(UserSubscriptionPreferences)
+admin.site.register(UserSocialConnection)
